de056d31d07da93d73d4797bc83c9f4cc2849dd1
Wolfeye3275 <sicolance@hotmail.com>
2019-11-23 07:21:45 -0800

    Made levels 1-3: Unity 2019.2.13f1

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,39 +5,78 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    // Con Fig Vars
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float rcsThrust = 100f;
+
     // State Vars
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        HandleInput();
+        Thrust();
+        Rotate();
     }
 
-    private void HandleInput()
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if (audioSource && !audioSource.isPlaying) audioSource.Play();
         }
+        else { if (audioSource) audioSource.Stop(); }
+    }
 
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true;
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
         {
-            
+
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+        rigidBody.freezeRotation = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("SAFE");
+                // Do nothing
+                break;
+            case "Fuel":
+                print("FUEL");
+                // Add code for giving the player some fuel
+                break;
+            case "Finish":
+                print("WIN");
+                // Add code for clearing the level
+                break;
+            default:
+                print("DEAD");
+                // Add code for managing player death
+                break;
         }
     }
 }

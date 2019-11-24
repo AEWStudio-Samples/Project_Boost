bb9cb88d59cb1366c421133a52ff271ae386aae0
Wolfeye3275 <sicolance@hotmail.com>
2019-11-23 21:06:59 -0800

    Linked the levels and added level 4: Unity 2019.2.13f1

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    // Con Fig Vars
+    [SerializeField] bool waveOffset = false;
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
+    [SerializeField] float waveOffsetDivider = 1f;
+    [SerializeField] float waveOffsetAddition = 0f;
+
+    // State Vars
+    Vector3 startingPos;
+    float movementFactor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalcMovementFactor();
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+    }
+
+    private void CalcMovementFactor()
+    {
+        if (period == 0) return;
+        float cycles = Time.time / period;
+
+        const float tau = Mathf.PI * 2f;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = ManageOffset(rawSinWave);
+    }
+
+    private float ManageOffset(float rawSinWave)
+    {
+        if (!waveOffset) return rawSinWave;
+        if (waveOffsetDivider == 0) waveOffsetDivider = 1;
+        return rawSinWave / waveOffsetDivider + waveOffsetAddition;
+
+    }
+}
--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,43 +1,69 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     // Con Fig Vars
-    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float levelLoadDelay = 2f;
+
+    [Header("Thrust Levels")]
+    [SerializeField] float mainThrust = 10f;
     [SerializeField] float rcsThrust = 100f;
+
+    [Header("Audio Clips")]
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip clearLevel;
+    [SerializeField] AudioClip death;
+
+    [Header("Particle VFX")]
+    [SerializeField] ParticleSystem mainEngineVFX;
+    [SerializeField] ParticleSystem clearLevelVFX;
+    [SerializeField] ParticleSystem deathVFX;
 
     // State Vars
     Rigidbody rigidBody;
     AudioSource audioSource;
+    int curLevelIndex;
+
+    enum State { Alive, Dying, Transcending}
+    State state = State.Alive;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
+        curLevelIndex = SceneManager.GetActiveScene().buildIndex;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
-    }
-
-    private void Thrust()
-    {
-        if (Input.GetKey(KeyCode.Space))
+        if (state == State.Alive)
         {
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
-            if (audioSource && !audioSource.isPlaying) audioSource.Play();
+            RespondToThrustInput();
+            RespondToRotationInput();
         }
-        else { if (audioSource) audioSource.Stop(); }
     }
 
-    private void Rotate()
+    private void RespondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) { ApplyThrust(); }
+        else
+        {
+            if (audioSource) audioSource.Stop();
+            mainEngineVFX.Stop();
+        }
+    }
+
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        if (audioSource && !audioSource.isPlaying) audioSource.PlayOneShot(mainEngine);
+        mainEngineVFX.Play();
+    }
+
+    private void RespondToRotationInput()
     {
         rigidBody.freezeRotation = true;
 
@@ -59,24 +85,55 @@ public class Rocket : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive) return; // Ignore collision when dead or transcending
+        if (audioSource) audioSource.Stop();
+        mainEngineVFX.Stop();
+
         switch (collision.gameObject.tag)
         {
-            case "Friendly":
-                print("SAFE");
-                // Do nothing
-                break;
-            case "Fuel":
-                print("FUEL");
-                // Add code for giving the player some fuel
+            case "Friendly": // Do nothing
                 break;
             case "Finish":
-                print("WIN");
-                // Add code for clearing the level
+                HandleVictory();
                 break;
             default:
-                print("DEAD");
-                // Add code for managing player death
+                HandleDeath();
                 break;
         }
+    }
+
+    private void HandleVictory()
+    {
+        state = State.Transcending;
+        audioSource.PlayOneShot(clearLevel);
+        clearLevelVFX.Play();
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    private void HandleDeath()
+    {
+        state = State.Dying;
+        audioSource.PlayOneShot(death);
+        deathVFX.Play();
+        Invoke("RestartLevel", levelLoadDelay);
+    }
+
+    private void LoadNextLevel()
+    {
+        if (curLevelIndex + 1 == SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else { SceneManager.LoadScene(curLevelIndex + 1); }
+    }
+
+    private void RestartLevel() // Implement later to restart level on death
+    {
+        SceneManager.LoadScene(curLevelIndex);
+    }
+
+    private void LoadFirstLevel() // Use to restart the game
+    {
+        SceneManager.LoadScene(0);
     }
 }
